65c95bfe2a0bec84a416b54e6c9e8616d610fa1b
AKHIL CHAPPIDI <achappidi@unomaha.edu>
2021-09-12 22:16:16 -0500

    Update settings.py

--- efs/settings.py
+++ efs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '96dl2+o7x08)%kuj74p)k(cp4jcnttgw!h0v98v3)yv&uw4w&*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
